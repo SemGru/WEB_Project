5bfcdd29ed6841fa367e8ed1bad5c580fd566af5
SemGru <s.m.grushko@gmail.com>
2024-03-31 23:30:44 +0300

    start

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import random
 from flask import Flask, url_for, request
 
 app = Flask(__name__)
-
+NAME = []
 
 @app.route('/', methods=['POST', 'GET'])
-@app.route('/astronaut_selection', methods=['POST', 'GET'])
+@app.route('/start', methods=['POST', 'GET'])
 def astronaut_selection():
     if request.method == 'GET':
         return f'''<!doctype html>
@@ -19,13 +19,13 @@ def astronaut_selection():
                     <title>Отбор астронавтов</title>
                   </head>
                   <body>
-                    <h1 align="center">Анкета претендента</h1>
-                    <h3 align="center">на участие в миссии</h3>
+                    <h1 align="center">Проверочная работа</h1>
+                    <h3 align="center">по информатике</h3>
                     <div>
                         <form class="login_form" method="post">
                             <input type="text" class="form-control" id="surname" aria-describedby="surnamelHelp" placeholder="Введите фамилию" name="surname">
                             <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name">
-                            <input type="text" class="form-control" id="class" aria-describedby="classHelp" placeholder="Введите класс" class="class">
+                            <input type="text" class="form-control" id="class" aria-describedby="classHelp" placeholder="Введите класс в формате 9 А" name="class">
                             <br>
                             <button type="submit" class="btn btn-primary">Начать</button>
                         </form>
@@ -33,26 +33,58 @@ def astronaut_selection():
                   </body>
                 </html>'''
     if request.method == 'POST':
-        print(request.form)
-        img = random.choice(['/static/img/Background_3.jpg', '/static/img/maars.jpg'])
-        return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Отбор астронавтов</title>
-                  </head>
-                  <body>
-                  <img src="{img}">
-                  <div class="form-group">
-                                <label for="about">Решение:</label>
-                                <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                  </div>
-                  <button type="submit" class="btn btn-primary">Отправить</button>
-                  <body>
-                </html>'''
+        NAME = request.form
+        print(NAME)
+        return tasks()
+        # img = random.choice(['/static/img/Background_3.jpg', '/static/img/maars.jpg'])
+        # return f'''<!doctype html>
+        #         <html lang="en">
+        #           <head>
+        #             <meta charset="utf-8">
+        #             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        #             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        #             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+        #             <title>Отбор астронавтов</title>
+        #           </head>
+        #           <body>
+        #           <img src="{img}">
+        #           <div class="form-group">
+        #                         <label for="about">Решение:</label>
+        #                         <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+        #           </div>
+        #           <button type="submit" class="btn btn-primary">Отправить</button>
+        #           <body>
+        #         </html>'''
+
+
+@app.route('/tasks', methods=['POST', 'GET'])
+def tasks():
+    # if request.method == 'GET':
+    # прикручитить получание названия файло виз БД
+    img = random.choice(['/static/img/Background_3.jpg', '/static/img/maars.jpg'])
+    # удаление названия файла чтобы изюежать повтора
+    return f'''<!doctype html>
+                    <html lang="en">
+                        <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Отбор астронавтов</title>
+                        </head>
+                        <body>
+                        <img src="{img}">
+                        <div class="form-group">
+                                        <label for="about">Решение:</label>
+                                        <textarea class="form-control" id="about" rows="5" name="about"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Отправить</button>
+                        <body>
+                    </html>'''
+    # if request.method == 'POST':
+    #     print(NAME)
+    #     print(request.form)
+    #     tasks()
 
 
 if __name__ == '__main__':
--- trash/trash_1.py
+++ trash/trash_1.py
@@ -1,95 +1,82 @@
-from flask import Flask, url_for, request
+from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/astronaut_selection', methods=['POST', 'GET'])
-def astronaut_selection():
-    if request.method == 'GET':
-        return f'''<!doctype html>
+@app.route('/')
+def start():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    text = ['Человечество вырастает из детства.',
+            'Человечеству мала одна планета.',
+            'Мы сделаем обитаемыми безжизненные пока планеты.',
+            'И начнем с Марса!',
+            'Присоединяйся!']
+    return return_sample_page()
+
+
+@app.route('/image_mars')
+def return_sample_page():
+    text = f"""<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='maars.jpg')}"
+                    alt="Вот она какая, красная планета.">
+                    <p>Вот она какая, красная планета.</p>
+                  </body>
+                </html>"""
+    return text
+
+
+@app.route('/promotion_image')
+def bootstrap():
+    return f'''<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Отбор астронавтов</title>
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}" />
+                    <title>Колонизация</title>
                   </head>
                   <body>
-                    <h1 align="center">Анкета претендента</h1>
-                    <h3 align="center">на участие в миссии</h3>
-                    <div>
-                        <form class="login_form" method="post">
-                            <input type="text" class="form-control" id="surname" aria-describedby="surnamelHelp" placeholder="Введите фамилию" name="surname">
-
-                            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name">
-                            <br>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                            <div class="form-group">
-                                <label for="eduSelect">Какое у Вас образование?</label>
-                                <select class="form-control" id="classSelect" name="edu">
-                                  <option>Начальное</option>
-                                  <option>Среднее</option>
-                                  <option>Выше среднего</option>
-                                  <option>Супер!</option>
-                                </select>
-                              </div>
-                                <label for="eduSelect">Какие у Вас есть профессии?</label>
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="prof" name="prof">
-                                <label class="form-check-label" for="acceptRules">Инженер-исследователь</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof1">
-                                <label class="form-check-label" for="acceptRules">Инженер-строитель</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof2">
-                                <label class="form-check-label" for="acceptRules">Пилот</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof3">
-                                <label class="form-check-label" for="acceptRules">Метеоролог</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof4">
-                                <label class="form-check-label" for="acceptRules">Инженер по жизнеобеспечению</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof5">
-                                <label class="form-check-label" for="acceptRules">Инженер по радиационной защите</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof6">
-                                <label class="form-check-label" for="acceptRules">Врач</label>
-                                <br><input type="checkbox" class="form-check-input" id="prof" name="prof7">
-                                <label class="form-check-label" for="acceptRules">Экзобиолог</label>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="form-check">Укажите пол</label>
-                                <div class="form-check">
-                                  <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                  <label class="form-check-label" for="male">
-                                    Мужской
-                                  </label>
-                                </div>
-                                <div class="form-check">
-                                  <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                  <label class="form-check-label" for="female">
-                                    Женский
-                                  </label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="about">Почему Вы хотите принять участие в миссии?</label>
-                                <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="photo">Приложите фотографию</label>
-                                <input type="file" class="form-control-file" id="photo" name="file">
-                            </div>
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Отправить</button>
-                        </form>
+                    <h1 style="color: red">Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='maars.jpg')}"
+                    alt="Картинку сьел яндекс. Добавьте свою">
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
                     </div>
                   </body>
                 </html>'''
-    if request.method == 'POST':
-        print(request.form)
-        return 'Форма отправлена'
 
 
 if __name__ == '__main__':

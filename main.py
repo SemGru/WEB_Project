0b608bd75f53cf2ffe511da646de39dbb7d16448
SemGru <s.m.grushko@gmail.com>
2024-04-07 19:32:53 +0300

    start

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
 NAME = []
+
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/start', methods=['POST', 'GET'])
 def astronaut_selection():
     if request.method == 'GET':
+        # required
+        # return render_template('start.html')
         return f'''<!doctype html>
                 <html lang="en">
                   <head>
@@ -16,18 +19,21 @@ def astronaut_selection():
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Отбор астронавтов</title>
+                    <title>Проверочная работа</title>
                   </head>
                   <body>
                     <h1 align="center">Проверочная работа</h1>
                     <h3 align="center">по информатике</h3>
                     <div>
                         <form class="login_form" method="post">
-                            <input type="text" class="form-control" id="surname" aria-describedby="surnamelHelp" placeholder="Введите фамилию" name="surname">
-                            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name">
-                            <input type="text" class="form-control" id="class" aria-describedby="classHelp" placeholder="Введите класс в формате 9 А" name="class">
+
+                            <input type="text" class="form-control" id="surname" aria-describedby="surnamelHelp" placeholder="Введите фамилию" name="surname" >
+                            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name" >
+                            <input type="text" class="form-control" id="class" aria-describedby="classHelp" placeholder="Введите класс в формате 9 А" name="class" >
                             <br>
-                            <button type="submit" class="btn btn-primary">Начать</button>
+                            <input type="text" class="form-control" id="var" aria-describedby="classHelp" placeholder="Введите вариант работы" name="var">
+                            <br>
+                             <button type="submit" class="btn btn-primary" Centered button >Начать</button>
                         </form>
                     </div>
                   </body>
@@ -36,33 +42,14 @@ def astronaut_selection():
         NAME = request.form
         print(NAME)
         return tasks()
-        # img = random.choice(['/static/img/Background_3.jpg', '/static/img/maars.jpg'])
-        # return f'''<!doctype html>
-        #         <html lang="en">
-        #           <head>
-        #             <meta charset="utf-8">
-        #             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        #             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        #             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-        #             <title>Отбор астронавтов</title>
-        #           </head>
-        #           <body>
-        #           <img src="{img}">
-        #           <div class="form-group">
-        #                         <label for="about">Решение:</label>
-        #                         <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-        #           </div>
-        #           <button type="submit" class="btn btn-primary">Отправить</button>
-        #           <body>
-        #         </html>'''
 
 
 @app.route('/tasks', methods=['POST', 'GET'])
 def tasks():
     # if request.method == 'GET':
     # прикручитить получание названия файло виз БД
-    img = random.choice(['/static/img/Background_3.jpg', '/static/img/maars.jpg'])
-    # удаление названия файла чтобы изюежать повтора
+    img = random.choice(['/static/img/1280.gif', '/static/img/1267.gif'])
+    # удаление названия файла чтобы избежать повтора
     return f'''<!doctype html>
                     <html lang="en">
                         <head>
@@ -70,15 +57,17 @@ def tasks():
                         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                         <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                        <title>Отбор астронавтов</title>
+                        <title>Проверочная работа</title>
                         </head>
                         <body>
-                        <img src="{img}">
+                        <form class="work_form" method="post">
+                        <img class="displayed" src="{img}">
                         <div class="form-group">
                                         <label for="about">Решение:</label>
-                                        <textarea class="form-control" id="about" rows="5" name="about"></textarea>
+                                        <textarea class="form-control" id="about" rows="5" name="decision"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-primary">Отправить</button>
+                        <button type="submit" class="btn btn-primary" Centered button >Отправить</button>
+                        </form>
                         <body>
                     </html>'''
     # if request.method == 'POST':
